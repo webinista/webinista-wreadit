8f74bbb78aed49ff05ed8c2eaa80ffc5304149ab
Tiffany Brown <tiffany@webinista.com>
2025-07-15 12:37:00 -0700

    Closes #2: Adds unit tests for WreadItIcon.jsx

---
+++ src/tests/WreaditIcon.test.js
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {
+  describe,
+  expect,
+  test,
+  beforeEach
+} from '@jest/globals';
+import {screen} from '@testing-library/dom';
+import {
+  render,
+  getByText
+} from '@testing-library/react';
+import WreadItIcon from '../components/WreadItIcon';
+
+const attributes = [
+  { fill: '#000' },
+  { fill: 'green' }
+];
+
+describe('WreadItIcon is rendered', () => {
+  test('can find ID', () => {
+
+    const icon = render( <WreadItIcon {...attributes[0] }/> );
+
+    expect(icon.getByTestId('wreadit_icon').getAttribute('id'))
+    .toBe('webinista_WreaditIcon');
+
+  });
+});
+
+describe('WreadItIcon fill color is ...', () => {
+
+  attributes.forEach((attr) => {
+    test( attr.fill , () => {
+      const icon = render( <WreadItIcon {...attr }/> );
+      const elementFill = icon.getByTestId('wreadit_icon_fill').getAttribute('fill');
+
+      expect( elementFill )
+      .toBe( attr.fill );
+    });
+  });
+
+});
