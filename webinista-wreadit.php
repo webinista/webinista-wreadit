0d9e05d1647e638f8ae25d49c81fd96aae26dbea
Tiffany Brown <tiffany@webinista.com>
2025-09-04 16:39:18 -0700

    Update version number to 1.2.0

--- webinista-wreadit.php
+++ webinista-wreadit.php
@@ -7,7 +7,7 @@
  * @link        https://webinista.com/wread-it/
  * Plugin Name:       Webinista WreadIt
  * Description:       Turn your posts into audio with Webinista WreadIt and AWS Polly.
- * Version:           1.1.2
+ * Version:           1.2.0
  * Requires at least: 6.7
  * Requires PHP:      8.0
  * Author:            Webinista, Inc.
