e84366f559dced533da511338215b26fee22e10e
Tiffany Brown <tiffany@webinista.com>
2025-09-04 17:11:16 -0700

    Remove unused code from scoper.inc.php.

--- scoper.inc.php
+++ scoper.inc.php
@@ -5,13 +5,6 @@ return [
 	// Patch files that have been processed to fix any missed or incorrectly prefixed strings.
 	'patchers' => [
 		static function ( string $filePath, string $prefix, string $contents ): string {
-			/*if ( false !== strpos( $filePath, 'Aws/Signature/SignatureV4.php' ) ) {
-				return str_replace(
-					'Webinista\\\\WreadIt\\\\Aws3\\\\Ymd\\\\THis\\\\Z',
-					'Ymd\\\\THis\\\\Z',
-					$contents
-				);
-			} */
 
 			if ( false !== strpos( $filePath, 'gcp-build/sdk/vendor/composer/autoload_real.php' ) ) {
 				return str_replace(
